89dbef41bd1bf5e5f1499795bd8a37c46e37b826
Timo <developer.timo@gmail.com>
2021-01-21 10:40:06 +0900

    문제 풀이 수정(StringTokenizer 제외)

--- src/bjalgo/Q11022.java
+++ src/bjalgo/Q11022.java
@@ -3,14 +3,12 @@ package bjalgo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Q11022 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
         
         int repeat = Integer.parseInt(br.readLine());
         String input;
@@ -18,9 +16,8 @@ public class Q11022 {
         
         for(int i = 1; i <= repeat; i++) {
             input = br.readLine();
-            st = new StringTokenizer(input, " ");
-            num1 = Integer.parseInt(st.nextToken());
-            num2 = Integer.parseInt(st.nextToken());
+            num1 = input.charAt(0) - '0';
+            num2 = input.charAt(2) - '0';
             sb.append("Case #").append(i).append(": ").append(num1).append(" + ").append(num2).append(" = ").append(num1 + num2).append("\n");
         }
         System.out.print(sb);
