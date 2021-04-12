1e18927c5ced178e6340cd93ce46d680de3aa0dc
Timo <developer.timo@gmail.com>
2021-04-12 12:43:55 +0900

    템플릿 메서드 패턴

---
+++ src/designpattern/templateMethod/Cat.java
@@ -0,0 +1,16 @@
+package designpattern.templateMethod;
+
+public class Cat extends Animal{
+
+    // 추상 메서드 오버라이딩
+    @Override
+    void play() {
+        System.out.println("야옹~ 야옹~");
+    }
+
+    // Hook(갈고리) 메서드 오버라이딩
+    @Override
+    void runSomething() {
+        System.out.println("야옹~ 야옹~ 꼬리 살랑 살랑~");
+    }
+}
