1511d095d272e8062b822fb3ad7dda28ec95a8eb
Timo <developer.timo@gmail.com>
2021-04-12 12:25:23 +0900

    데코레이터 패턴

---
+++ src/designpattern/decorator/ClientWithDecorator.java
@@ -0,0 +1,10 @@
+package designpattern.decorator;
+
+import designpattern.proxy.IService;
+
+public class ClientWithDecorator {
+    public static void main(String[] args) {
+        IService decorator = new Decorator();
+        System.out.println(decorator.runSomething());
+    }
+}
